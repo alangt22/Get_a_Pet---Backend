ee8371d920f7f32c60313605cd0967d1f9e16534
alangt22 <alansilva2896@gmail.com>
2024-11-28 21:25:16 -0300

    update 12

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 // solve cors
-app.use(cors({credentials: true, origin: 'https://getapet-backend-production.up.railway.app/'}))
+app.use(cors({credentials: true, origin: 'http://localhost:5000'}))
 
 // public folder for images
 app.use(express.static('public'))
