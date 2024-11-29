20a19b3f3c1039f5fb9ea03566ff07b1ea9cb872
alangt22 <alansilva2896@gmail.com>
2024-11-28 21:28:28 -0300

    update 15

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-// config JSON response
-app.use(express.json())
+// Configuração do CORS para permitir origens específicas
+app.use(cors({
+  credentials: true,
+  origin: ['https://get-a-pet-aln.netlify.app', 'http://localhost:5136']  // Permite tanto a origem de produção quanto o localhost
+}));
 
-// solve cors
-app.use(cors({credentials: true, origin: 'http://localhost:5000'}))
+// Configuração para resposta em JSON
+app.use(express.json());
 
-// public folder for images
-app.use(express.static('public'))
+// Servindo arquivos estáticos
+app.use(express.static('public'));
 
-//routes
-const UserRoutes = require('./routes/UserRoutes')
-const PetRoutes = require('./routes/PetRoutes')
+// Rotas
+const UserRoutes = require('./routes/UserRoutes');
+const PetRoutes = require('./routes/PetRoutes');
 
-app.use('/users', UserRoutes )
-app.use('/pets', PetRoutes )
+app.use('/users', UserRoutes);
+app.use('/pets', PetRoutes);
 
+// Iniciar o servidor
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000');
+});
 
-app.listen(5000)
+module.exports = app;
