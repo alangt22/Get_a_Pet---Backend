e890e640999df773d803bfd322ba9f8df6693ee9
alangt22 <alansilva2896@gmail.com>
2024-11-28 21:16:00 -0300

    update 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/users", UserRoutes);
 app.use("/pets", PetRoutes);
 
 // Configuração da porta
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5136;
 
 app.listen(PORT, function() {
   console.log(`Servidor Online na porta ${PORT}!`);
