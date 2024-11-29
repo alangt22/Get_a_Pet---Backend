39efadf78f22c08957c6e6486aa83f0327310420
alangt22 <alansilva2896@gmail.com>
2024-11-28 21:04:06 -0300

    update

--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect('mongodb+srv://alan28:123@cluster0.ra1hq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  console.log('Conectou com Mongoose!')
+    try {
+        mongoose.set("strictQuery", true);
+
+
+        const uri = "mongodb+srv://alan28:123@cluster0.ra1hq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+        await mongoose.connect(uri);
+
+        console.log("Conectado ao banco de dados com sucesso");
+    } catch (error) {
+        console.error("Erro ao conectar ao banco de dados:", error.message);
+    }
 }
 
-main().catch((err) => console.log(err))
+module.exports = main;
 
-module.exports = mongoose
+
+
 
