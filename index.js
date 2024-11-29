b8053757dd1ae5a0c33d6846f0387d119f2f8ee3
alangt22 <alansilva2896@gmail.com>
2024-11-28 21:10:21 -0300

    update 1

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const app = express()
+// Configuração do CORS
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:5173"
+}));
 
-// config JSON response
-app.use(express.json())
+// Configuração para resposta em JSON
+app.use(express.json());
 
-// solve cors
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
+// Conexão com o banco de dados
+// Se você tiver uma função de conexão com banco de dados, pode colocá-la aqui, por exemplo:
+// const conn = require("./db/conn.js");
+// conn();
 
-// public folder for images
-app.use(express.static('public'))
+// Rotas
+const UserRoutes = require("./routes/UserRoutes");
+const PetRoutes = require("./routes/PetRoutes");
 
-//routes
-const UserRoutes = require('./routes/UserRoutes')
-const PetRoutes = require('./routes/PetRoutes')
+app.use("/users", UserRoutes);
+app.use("/pets", PetRoutes);
 
-app.use('/users', UserRoutes )
-app.use('/pets', PetRoutes )
+// Configuração da porta
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, function() {
+  console.log(`Servidor Online na porta ${PORT}!`);
+});
 
-app.listen(5000)
+module.exports = app;
